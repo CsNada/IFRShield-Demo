ab2726eb83a37725f2708c67d760c5960451db23
CsNada <150547564+CsNada@users.noreply.github.com>
2026-07-16 20:10:00 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -13,11 +13,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://ifrshield-demo-blue.vercel.app",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
