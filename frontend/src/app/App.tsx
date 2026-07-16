a9dc752acc7eac82a28a4337f5b868be6f083689
CsNada <150547564+CsNada@users.noreply.github.com>
2026-07-16 20:29:16 +0300

    Update App.tsx

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { LanguageProvider } from "./context/language";
+import ScrollToTop from "../components/ScrollToTop";
 
 export default function App() {
   return (
     <LanguageProvider>
+      <ScrollToTop />
       <RouterProvider router={router} />
     </LanguageProvider>
   );
